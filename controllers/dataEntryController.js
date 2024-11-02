b2ae247b114ce0bafdfe9ab63d2d192b6080cff0
Fabiha112 <fabihatabassum119@gmail.com>
2024-11-02 15:59:23 +0600

    added channel name

--- controllers/dataEntryController.js
+++ controllers/dataEntryController.js
@@ -85,62 +85,6 @@ class DataController {
     }
    
   };
-//   // Fetch only numeric values for a specific user by userId
-//   static getAllNumbericFieldsByUserId = async(req,res) => {
-//     try{
-//       // Get the userId from the authenticated request
-//       const userId = req.user._id;
-//       console.log('Fetching data for user ID:,userId');
-//     // Find all entries for the user by userId
-//       const userEntries = await DataModel.find({userId:userId});
-//       // If no entry is found, return a 404 error
-//       if(!userEntries || userEntries.length ===0){
-//         return res.status(404).json({status:"failed",message:"User entry not found"});
-//       }
-//             // Array to hold numeric fields from all entries
-//             const allNumericFields = userEntries.map(entry => {
-//               const numericFields = {
-//                 wakingUp: entry.wakingUp,
-//                 firstGoOut: entry.firstGoOut,
-//                 firstScreenOn: entry.firstScreenOn,
-//                 breakfast: entry.breakfast,
-//                 lunch: entry.lunch,
-//                 eveningSnacks: entry.eveningSnacks,
-//                 dinner: entry.dinner,
-//                 goingToSleep: entry.goingToSleep,
-//                 cooperateAtHome: entry.cooperateAtHome,
-//                 overnightSleeping: entry.overnightSleeping,
-//                 gettingSleepTime: entry.gettingSleepTime,
-//                 outgoingTendency: entry.outgoingTendency,
-//                 outgoingCount: entry.outgoingCount,
-//                 screenTime: entry.screenTime,
-//                 junkFood: entry.junkFood,
-//                 makingNoise: entry.makingNoise,
-//                 walking: entry.walking,
-//                 showingAnger: entry.showingAnger,
-//                 glassCrashTendency: entry.glassCrashTendency,
-//                 pushingTendency: entry.pushingTendency,
-//                 itemThrowTendency: entry.itemThrowTendency,
-//                 foodWaterThrowTendency: entry.foodWaterThrowTendency,
-//                 hitWithHand: entry.hitWithHand,
-//                 hitWithHead: entry.hitWithHead,
-//                 masturbation: entry.masturbation
-//               };
-          
-
-// return {
-//   dateOfRecord: entry.dateOfRecord,
-//   numericFields: numericFields
-// };
-// });
-
-//  // Return only the numeric fields
-//  res.status(200).json({ status: "success", data: allNumericFields });
-// } catch (error) {
-//   console.error('Error fetching numeric fields:', error);
-//   res.status(500).json({ status: "failed", message: "Failed to fetch numeric fields" });
-// }
-// };
 // Fetch specific numeric fields for a user by userId and calculate mean and standard deviation
 static getSelectedNumericFieldsWithStatsByUserId = async (req, res) => {
   try {
